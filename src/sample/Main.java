2593b33c234330e510dbb5efb197f60cd796f4be
meach <meachkaddour1@gmail.com>
2021-01-08 16:21:40 +0100

    update folder

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Program Geastion des Etudiant");
         primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.show();
 
